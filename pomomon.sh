1ecaf2b57ba910e7aa1d894192ede691873bd009
Tadas <tadas@truebill.com>
2021-03-06 20:27:14 -0500

    add Darwin support for pomomon.sh

--- pomomon.sh
+++ pomomon.sh
@@ -3,10 +3,16 @@
 # pomo status monitor. Sends notification and stops the timer once the end is
 # reached.
 
+os=$(uname)
+
 while true ; do
 	status=$(pomo status | tr '\n' ' ')
 	if [ "${status:0:1}" == "-" ]; then
-		notify-send pomo "pomodoro timer ended"
+		if [ "$os" == "Darwin" ]; then
+			osascript -e 'display notification "pomodoro timer ended!"'
+		else
+			notify-send pomo "pomodoro timer ended"
+		fi
 		pomo stop
 	fi
 	sleep 1
